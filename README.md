dbf3aa8fe7a2be32a5d5e236b46d25b031c355d2
Areng1234 <112800172+Areng1234@users.noreply.github.com>
2026-01-15 08:22:52 +0700

    stuff

---
+++ README.md
@@ -0,0 +1,2 @@
+### Minimal Chess BACKEND
+Use socket connections from the nextjs fromend to make moves here.
--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import { Chess } from 'chess.js';
 
 function createGame() {
+    // Creates the game and return a gameid (Where to connect to)
     const chess = new Chess();
     return chess
 }
